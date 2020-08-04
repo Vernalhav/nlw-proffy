83edaef5895305cd7472ab584cf5477cf2a74131
Victor Giovannoni <vernalhav@usp.br>
2020-08-03 23:23:18 -0300

    Starting implementation of Study screen

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImage from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css';
+
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+
+        </div>
+    );
+}
+
+export default TeacherList;
